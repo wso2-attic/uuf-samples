eb43edbe58f3c54f4664996d52b6d5c0a3314aff
manuranga <manu@wso2.com>
2016-03-03 12:52:06 +0530

    adding sample 1

---
+++ pet-store/pages/pet/pet.js
@@ -0,0 +1,7 @@
+function onRequest(context) {
+    var pet = callService('GET','/' + context.uriParams.id);
+    if(!pet.img) {
+        pet.img = context.app.context + '/public/app/images/pet.png';
+    }
+    return  pet;
+}
