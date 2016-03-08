8a82121ed604e1743a37dbc317aff79142b11db0
manuranga <manu@wso2.com>
2016-03-08 10:32:29 +0530

    removing duplicates

--- pet-store-non-working/pages/pets/*.js
+++
@@ -1,7 +0,0 @@
-function onRequest(context) {
-    var pet = callService('GET','/' + context.uriParams.id);
-    if(!pet.img) {
-        pet.img = context.app.context + '/public/app/images/pet.png';
-    }
-    return  pet;
-}
