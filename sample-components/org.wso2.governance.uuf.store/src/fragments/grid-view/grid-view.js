5d5551008c381d02b19229dacacad8c2a1dfb156
sajithar <sajithar@wso2.com>
2016-03-17 18:45:06 +0530

    added store component sample

---
+++ sample-components/org.wso2.governance.uuf.store/src/fragments/grid-view/grid-view.js
@@ -0,0 +1,20 @@
+function onRequest(context) {
+    return [
+        {
+            id: 123456,
+            name: "Test Asset",
+            fields: [
+                {name: "Author", value: "admin"},
+                {name: "Created Date", value: "2016/03/17"}
+            ]
+        },
+        {
+            id: 123456,
+            name: "Test Asset",
+            fields: [
+                {name: "Author", value: "admin"},
+                {name: "Created Date", value: "2016/03/17"}
+            ]
+        }
+    ];
+}
